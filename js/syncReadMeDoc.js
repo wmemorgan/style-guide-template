3e443b439c612f7939511f8a4fd30d8974b8c252
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-26 14:08:48 +0800

    Add script to synchronize the official documentation within the style guide

---
+++ js/syncReadMeDoc.js
@@ -0,0 +1,14 @@
+
+let url = "http://wilfredmorgan.com/style-guide-template/README.md"
+
+async function getData(url) {
+  let downloadData = await fetch(url)
+  let data = await downloadData.text()
+  document.querySelector('.overview').innerHTML = marked(data)
+}
+
+window.onload = getData(url)
+
+
+
+
